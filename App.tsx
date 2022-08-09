b58c1a3b37867b0460eb28ba9d7b7815ea1c26fe
Yakup Özbaydar <55889068+yakupozbaydar@users.noreply.github.com>
2022-08-09 17:39:55 +0300

    document picker
    added needs to be fixed

--- App.tsx
+++ App.tsx
@@ -1,64 +1,21 @@
-import React, { useState } from 'react';
-import { StyleSheet, Dimensions, View, Text } from 'react-native';
-import Pdf from 'react-native-pdf';
-import PageOrganizer from './PageOrganizer';
 
-const PDFExample = () => {
-  const source = {
-    uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
-    cache: true,
-  };
-  //const source = require('./test.pdf');  // ios only
-  //const source = {uri:'bundle-assets://test.pdf' };
-  //const source = {uri:'file:///sdcard/test.pdf'};
-  //const source = {uri:"data:application/pdf;base64,JVBERi0xLjcKJc..."};
-  //const source = {uri:"content://com.example.blobs/xxxxxxxx-...?offset=0&size=xxx"};
-  //const source = {uri:"blob:xxxxxxxx-...?offset=0&size=xxx"};
-  let curPage = 0;
-  let number = 0;
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './Home';
+import PDFView from './PDFView';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Pdf
-        trustAllCerts={false}
-        source={source}
-        onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`Number of pages: ${numberOfPages}`);
-          number = numberOfPages;
-        }}
-        onPageChanged={(page, numberOfPages) => {
-          console.log(`Current page: ${page}`);
-          curPage = page;
-        }}
-        onError={error => {
-          console.log(error);
-        }}
-        onPressLink={uri => {
-          console.log(`Link pressed: ${uri}`);
-        }}
-        style={styles.pdf}
-        enablePaging
-        enableAntialiasing
-        enableRTL	
-        enableAnnotationRendering
-      />
-      <Text>
-        {curPage}/{number}
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="PDFView" component={PDFView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
-export default PDFExample;
+export default App;
